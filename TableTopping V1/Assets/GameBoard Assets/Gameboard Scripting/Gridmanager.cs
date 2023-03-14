008b9130a172f4b7900ab00ddae3b46ce87bda97
mboddicker3 <101283085+mboddicker3@users.noreply.github.com>
2023-03-13 20:54:35 -0400

    Fixed some issues on Gridmanager.cs
    simple debugging

--- TableTopping V1/Assets/GameBoard Assets/Gameboard Scripting/Gridmanager.cs
+++ TableTopping V1/Assets/GameBoard Assets/Gameboard Scripting/Gridmanager.cs
@@ -6,21 +6,21 @@ public class Gridmanager : MonoBehaviour
 {
 
     //https://www.youtube.com/watch?v=kkAjpQAM-jE&ab_channel=Tarodev video for creating grid in unity 2:20 in
-    [SerializedField] private int _width, _height;
+    [SerializeField] private int _width, _height;
 
-    [SerializedField] private Tile _tilePrefab;
+    [SerializeField] private Tile _tilePrefab;
 
     void Start(){
         GenerateGrid();
     }
 
-    void GenerateGrid{
+    void GenerateGrid(){
         for(int x = 0; x < _width; x++){
-        for(int y = 0; y < _height; y++){
-            var spawnedTile = instantiate(_tilePrefab, new Vector3(x,z), Quaternion.identity);
-            spawnedTile.name = $"Tile {x} {y}";
+            for(int y = 0; y < _height; y++){
+                var spawnedTile = instantiate(_tilePrefab, new Vector3(x,y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+            }
         }
-    }
     }
     
 }
